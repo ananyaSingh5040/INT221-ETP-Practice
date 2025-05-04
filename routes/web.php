37143b6be82f1642de669e8bc2721e186413234e
Ananya Singh <singhananya5040@gmail.com>
2025-05-04 12:57:40 +0530

    04-05-25

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\formController;
+use App\Http\Controllers\studentController;
+use App\Http\Controllers\TableController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use PhpParser\Node\Expr\FuncCall;
@@ -14,17 +17,46 @@ Route::get('/dynamic/{name}', function ($name) {
     return view('dynamicParam', ['name' => $name]);
 });
 
-Route::view('/about','about');
+Route::view('/about', 'about');
 // Route::redirect('/about','/');
 
-Route::get('user',[UserController::class,'getUser']);
+Route::get('user', [UserController::class, 'getUser']);
 
 // Dynamic with controller
-Route::get('user/{name}',[UserController::class,'getUserName']);
+Route::get('user/{name}', [UserController::class, 'getUserName']);
 
-Route::get('/message',function(){
+Route::get('/message', function () {
     return view('messageViewer');
 });
 
-Route::view('userForm','userForm');
-Route::post('userForm',[UserController::class,'userForm']);
+Route::view('userForm', 'userForm');
+Route::post('userForm', [UserController::class, 'userForm']);
+
+Route::view('form', 'formView');
+Route::post('adduser', [formController::class, 'getFormData']);
+
+Route::view('url1', 'UrlTesting1');
+Route::view('url2', 'UrlTesting2');
+
+// Named Route:
+Route::view('namedView/profile/user', 'namedView')->name('hm');
+Route::get('show', [UserController::class, 'show']);
+
+Route::prefix('student')->group(function () {
+
+    Route::get('show', [studentController::class, 'show']);
+    Route::get('add', [studentController::class, 'add']);
+});
+
+
+Route::controller(studentController::class)->group(function(){
+
+    Route::get('showCont','show');
+    Route::get('addCont','add');
+    Route::get('aboutCont/{name}','about');
+
+});
+
+Route::view('ageCheck','welcome')->middleware('check1');
+
+Route::get('getUsers',[TableController::class,'showUsers']);
